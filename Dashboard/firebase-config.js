ced52f6401c78199b239133749e2b49bd4adb808
Karthikeyan M <karthikeya732006@gmail.com>
2026-08-11 01:34:32 +0530

    Update firebase-config.js

--- Dashboard/firebase-config.js
+++ Dashboard/firebase-config.js
@@ -3,17 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ80wPekkKX3-bY0f4l5YNWOCs72hLQPE",
-  authDomain: "smart-transformer-digital-twin.firebaseapp.com",
-  databaseURL: "https://smart-transformer-digital-twin-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smart-transformer-digital-twin",
-  storageBucket: "smart-transformer-digital-twin.firebasestorage.app",
-  messagingSenderId: "181417968589",
-  appId: "1:181417968589:web:8c1ac43ebbfe57a5d5fa84"
+    apiKey: "AIzaSyDQ80wPekkKX3-bY0f4l5YNWOCs72hLQPE",
+    authDomain: "smart-transformer-digital-twin.firebaseapp.com",
+    databaseURL: "https://smart-transformer-digital-twin-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smart-transformer-digital-twin",
+    storageBucket: "smart-transformer-digital-twin.firebasestorage.app",
+    messagingSenderId: "181417968589",
+    appId: "1:181417968589:web:8c1ac43ebbfe57a5d5fa84"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getDatabase(app);
 
 export { db };
